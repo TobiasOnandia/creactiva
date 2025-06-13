bffb0700494360ce9be2624e2e7524c648a6bb2b
Tobias Onandia <tobiasonandia0@gmail.com>
2025-06-13 09:03:26 -0300

    better practice

--- components/canvas/CanvasItemContent.tsx
+++ components/canvas/CanvasItemContent.tsx
@@ -1,5 +1,4 @@
 import { ItemRenderers } from "@/components/factories/ItemRenderers";
-import { ElementConfig } from "@/types/canvas/CanvasTypes";
 import { Json } from "@/types/database/database.types";
 
 type CanvasItemType =
@@ -15,21 +14,22 @@ type CanvasItemType =
   | "select"
   | "checkbox"
   | "submit";
+
 interface CanvasItemContentProps {
   type: CanvasItemType | string;
-  config: ElementConfig;
-  id:string
+  config: Json;
+  id: string;
 }
 
-export const CanvasItemContent = ({ id,  type, config }: CanvasItemContentProps) => {
-  console.log(type)
-
+export const CanvasItemContent = ({
+  id,
+  type,
+  config,
+}: CanvasItemContentProps) => {
   const Renderer = ItemRenderers[type];
   if (Renderer) {
-        return <Renderer config={config as unknown as Json} id={id} />;
+    return <Renderer config={config} id={id} />;
   }
-
-
   return (
     <p className="text-sm text-neutral-300 text-center w-full h-full flex items-center justify-center p-2 border border-dashed border-neutral-600 rounded">
       Item: {type} - Contenido no definido
